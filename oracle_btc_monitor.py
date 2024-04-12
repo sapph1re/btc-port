00eddf0a14f54864d759ee513e80385676c82e8d
sapph1re <dev.romanv@gmail.com>
2024-04-12 01:18:07 -0600

    Separated monitoring Bitcoin Vault from notifying Solana Contract, using Redis to store txs

---
+++ oracle_btc_monitor.py
@@ -0,0 +1,96 @@
+import requests
+import redis
+import time
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Bitcoin address to monitor
+VAULT = os.getenv('BITCOIN_VAULT_ADDRESS')
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+
+def get_address_txs(address, from_block=0):
+    """Retrieve the list of transactions for the given address."""
+    url = f"https://bitcoinexplorer.org/api/address/{address}"
+    response = requests.get(url)
+    data = response.json()
+    txs = []
+    for txid in data.get('txHistory', {}).get('txids', []):
+        block = data.get('txHistory', {}).get('blockHeightsByTxid', {}).get(txid, 0)
+        if block <= from_block:
+            continue
+        txs.append({
+            'txid': txid,
+            'block': block,
+        })
+    return txs
+
+def get_tx(txid):
+    """Retrieve the transaction details for the given transaction ID."""
+    url = f"https://bitcoinexplorer.org/api/tx/{txid}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def get_tx_info(tx):
+    """Retrieve: user address, transaction hash, amount, is_incoming"""
+    txid = tx['txid']
+    block = tx['block']
+    tx = get_tx(txid)
+    sender = tx['vin'][0]['scriptSig']['address']
+    if sender == VAULT:
+        is_incoming = False
+        for vout in tx['vout']:
+            if vout['scriptPubKey']['addresses'][0] != VAULT:
+                user_address = vout['scriptPubKey']['addresses'][0]
+                amount = vout['value']
+                break
+    else:
+        is_incoming = True
+        user_address = sender
+        for vout in tx['vout']:
+            if vout['scriptPubKey']['addresses'][0] == VAULT:
+                amount = vout['value']
+                break
+    return {
+        'uaddr': user_address,
+        'txid': txid,
+        'amt': amount,
+        'in': is_incoming,
+        'block': block,
+    }
+
+def latest_processed_block():
+    """Get the latest processed transaction's block height from Redis"""
+
+    tx = r.lindex('vault_txs', {})
+    if tx:
+        return int(tx['block'])
+    return 0
+
+def run():
+    """Monitor transactions on Bitcoin vault and push them to Redis"""
+
+    # get the latest processed transaction's block height
+    last_block = latest_processed_block()
+
+    while True:
+        # get the latest transactions
+        for tx in get_address_txs(VAULT, last_block + 1):
+            tx_info = get_tx_info(tx)
+            print(f"New transaction: {tx_info}")
+            # push the transaction to Redis for further processing
+            r.lpush('vault_txs', tx_info)
+            # update the latest processed transaction's block height
+            if tx_info['block'] > last_block:
+                last_block = tx_info['block']
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    run()
---
+++ oracle_solana_notifier.py
@@ -0,0 +1,82 @@
+import redis
+import time
+import os
+from solana.rpc.api import Client
+from solana.transaction import Transaction
+from spl import Account, TransferParams, transfer, PublicKey
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Solana smart contract details
+SOLANA_CONTRACT_ADDRESS = os.getenv('SOLANA_CONTRACT_ADDRESS')
+SOLANA_PAYER_PRIVATE_KEY = os.getenv('SOLANA_PAYER_PRIVATE_KEY')
+
+# Connect to the Solana network
+SOLANA_RPC = os.getenv('SOLANA_RPC')
+client = Client(SOLANA_RPC)
+payer = Account(SOLANA_PAYER_PRIVATE_KEY)
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+
+def notify_sc(user_address, tx_hash, amount, is_incoming, block):
+    """Send transaction data to the Solana smart contract."""
+    transaction = Transaction()
+    transfer_params = TransferParams(
+        from_pubkey=payer.public_key(),
+        to_pubkey=PublicKey(SOLANA_CONTRACT_ADDRESS),
+        lamports=0,  # No need to transfer SOL for this transaction
+    )
+    instruction = transfer(params=transfer_params)
+    transaction.add(instruction)
+
+    # Encode transaction data
+    transaction_data = {
+        'user_address': user_address,
+        'tx_hash': tx_hash,
+        'amount': int(amount * 100_000_000),
+        'is_incoming': is_incoming,
+        'block': block,
+    }
+    transaction.add_instruction(
+        program_id=PublicKey(SOLANA_CONTRACT_ADDRESS),
+        data=bytes(str(transaction_data), 'utf-8'),
+        accounts=[],
+    )
+
+    transaction.sign(payer)
+    transaction_signature = transaction.signature()
+    print(f"Sending transaction data to Solana: {transaction_data}")
+    print(f"Transaction signature: {transaction_signature}")
+
+    # Send the transaction to the Solana network
+    client.send_transaction(transaction)
+
+def get_sc_latest_block():
+    """Retrieve the latest processed Bitcoin block height from the Solana smart contract."""
+    # TODO
+    return 0
+
+def run():
+    """Get new transactions from Redis and notify the Solana smart contract"""
+
+    from_block = get_sc_latest_block()
+    while True:
+        for tx in r.lrange('vault_txs', 0, -1):
+            if tx['block'] <= from_block:
+                continue
+            print(f"New transaction: {tx}")
+            notify_sc(
+                user_address=tx['uaddr'],
+                tx_hash=tx['txid'],
+                amount=tx['amt'],
+                is_incoming=tx['in'],
+                block=tx['block'],
+            )
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run()
