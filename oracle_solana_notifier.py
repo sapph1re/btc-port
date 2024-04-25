8d0c8d6bebe65ee1d914701a87ecfb051f3b448d
sapph1re <dev.romanv@gmail.com>
2024-04-24 20:17:56 -0500

    Solana Notifier: use AnchorPy client (WIP)

--- oracle_solana_notifier.py
+++ oracle_solana_notifier.py
@@ -1,88 +1,76 @@
-import pickle
-import redis
-import time
 import os
-from solana.rpc.api import Client
+import time
+import asyncio
 from solana.transaction import Transaction
-from spl import Account, TransferParams, transfer, PublicKey
+from solana.rpc.async_api import AsyncClient
+from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+from anchorpy import Program, Provider, Wallet
+from solana_program.client_py.program_id import PROGRAM_ID
+from solana_program.client_py.instructions import add_transaction
+from solana_program.client_py.accounts import BitcoinTransactionProcessor
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Solana smart contract details
-SOLANA_CONTRACT_ADDRESS = os.getenv('SOLANA_CONTRACT_ADDRESS')
+SOLANA_RPC = os.getenv('SOLANA_RPC')
+SOLANA_PROGRAM_ID = os.getenv('SOLANA_PROGRAM_ID')
 SOLANA_PAYER_PRIVATE_KEY = os.getenv('SOLANA_PAYER_PRIVATE_KEY')
 
-# Connect to the Solana network
-SOLANA_RPC = os.getenv('SOLANA_RPC')
-client = Client(SOLANA_RPC)
-payer = Account(SOLANA_PAYER_PRIVATE_KEY)
-
-r = redis.Redis(
-    host=os.getenv('REDIS_HOST'),
-    port=os.getenv('REDIS_PORT'),
-    db=os.getenv('REDIS_DB')
-)
+conn = AsyncClient(SOLANA_RPC)
+# payer = Account(SOLANA_PAYER_PRIVATE_KEY)
 
 
-def notify_sc(user_address, tx_hash, amount, is_incoming, block):
-    """Send transaction data to the Solana smart contract."""
-    transaction = Transaction()
-    transfer_params = TransferParams(
-        from_pubkey=payer.public_key(),
-        to_pubkey=PublicKey(SOLANA_CONTRACT_ADDRESS),
-        lamports=0,  # No need to transfer SOL for this transaction
-    )
-    instruction = transfer(params=transfer_params)
-    transaction.add(instruction)
-
-    # Encode transaction data
-    transaction_data = {
-        'user_address': user_address,
-        'tx_hash': tx_hash,
-        'amount': int(amount * 100_000_000),
-        'is_incoming': is_incoming,
-        'block': block,
-    }
-    transaction.add_instruction(
-        program_id=PublicKey(SOLANA_CONTRACT_ADDRESS),
-        data=bytes(str(transaction_data), 'utf-8'),
-        accounts=[],
-    )
-
-    transaction.sign(payer)
-    transaction_signature = transaction.signature()
-    print(f"Sending transaction data to Solana: {transaction_data}")
-    print(f"Transaction signature: {transaction_signature}")
-
-    # Send the transaction to the Solana network
-    client.send_transaction(transaction)
+def push_to_program(user_address, tx_hash, amount, is_incoming, block):
+    """Send transaction data to the Solana program."""
+    pass
+    # TODO
+    # ix = add_transaction({
+        # 'user_address': user_address,
+        # 'tx_hash': tx_hash,
+        # 'amount': int(amount * 100_000_000),
+        # 'is_incoming': is_incoming,
+        # 'block': block,
+    # }, {
+    #   "processor": processor_account,
+    #   "oracle_authority": payer.public_key(),
+    # })
+    # tx = Transaction().add(ix)
+    # provider = Provider.local()
+    # await provider.send(tx, [payer, acc])
+    # print(f"Sending transaction data to Solana: {transaction_data}")
+    # print(f"Transaction signature: {transaction_signature}")
 
 def get_sc_latest_block():
-    """Retrieve the latest processed Bitcoin block height from the Solana smart contract."""
+    """Retrieve the latest processed Bitcoin block height from the Solana program."""
     # TODO
     return 0
 
-def run():
-    """Get new transactions from Redis and notify the Solana smart contract"""
+async def main():
+    """Get new transactions from DB and push to the Solana program."""
 
-    from_block = get_sc_latest_block()
-    while True:
-        for tx in r.lrange('vault_txs', 0, -1):
-            tx = pickle.loads(tx)
-            if tx['block'] <= from_block:
-                continue
-            print(f"New transaction: {tx}")
-            notify_sc(
-                user_address=tx['uaddr'],
-                tx_hash=tx['txid'],
-                amount=tx['amt'],
-                is_incoming=tx['in'],
-                block=tx['block'],
-            )
-        time.sleep(1)
+    processor_acc_addr = Pubkey.find_program_address([bytes("processor", "utf-8")], PROGRAM_ID)[0]
+    print(processor_acc_addr)
+    processor_acc = await BitcoinTransactionProcessor.fetch(conn, processor_acc_addr)
+    print(processor_acc)
+
+    # from_block = get_sc_latest_block()
+    # while True:
+    #     for tx in r.lrange('vault_txs', 0, -1):
+    #         tx = pickle.loads(tx)
+    #         if tx['block'] <= from_block:
+    #             continue
+    #         print(f"New transaction: {tx}")
+    #         push_to_program(
+    #             user_address=tx['uaddr'],
+    #             tx_hash=tx['txid'],
+    #             amount=tx['amt'],
+    #             is_incoming=tx['in'],
+    #             block=tx['block'],
+    #         )
+    #     time.sleep(1)
 
 
 if __name__ == "__main__":
-    run()
+    asyncio.run(main())
